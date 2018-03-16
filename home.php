8f56df0a3a183c0441fec87970799d398c3c9eae
15pa1a05d1 <37134716+15pa1a05d1@users.noreply.github.com>
2018-03-16 17:47:19 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="home6.css" />
+</head>
+<body>
+        <div class='heading'
+        <h2 class='header'>NEWS</h2></div>
+       <div class='r'
+        <ul class="nav">
+            <li><a href="register6.php">Register</a></li>
+             <li><a href="login6.php">Login</a></li>
+         </ul></div>
+        
+        <img src="23-Cigma-in-Newspaper-2010-466x312.jpg">
+</body>
+</html>
